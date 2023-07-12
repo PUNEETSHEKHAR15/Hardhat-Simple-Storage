c3beecc5a9049c59e569677dfdbacf4979e7bf2f
PUNEET SHEKHAR <shekharpuneet19@gmail.com>
2023-07-13 00:48:44 +0530

    Created Repository for hardhat-Simple-Storage

---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+// import section // ethers is used to interact with the contracts and network 
+// main function 
+// calling main function 
+async function main() {
+
+}
+
+main().then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  })
